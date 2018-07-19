d0e8320675efd665091b0636abf8ee82f75a6321
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-07-19 14:27:48 +0200

    Added test for the source being unsubscribed

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,7 +1,12 @@
 import observe from 'callbag-observe'
 import pipe from 'callbag-pipe'
+import take from 'callbag-take'
+import tap from 'callbag-tap'
+import tapUp from 'callbag-tap-up'
 
 import of from '../src'
+
+const noop = () => {}
 
 test('works', () => {
   const actual = []
@@ -13,5 +18,33 @@ test('works', () => {
     }),
   )
 
-  return expect(actual).toEqual([10, 20, 30, 40, 50])
+  expect(actual).toEqual([10, 20, 30, 40, 50])
+})
+
+test('should stop emitting values (& completion) after being unsubscribed', done => {
+  const actual = []
+
+  let disposed = false
+
+  const failAfterDisposal = () => {
+    if (!disposed) return
+    done.fail(
+      'Nothing should be emitted down to the sink after the source gets unsubscribed.',
+    )
+  }
+
+  pipe(
+    of(10, 20, 30, 40, 50),
+    tapUp(noop, noop, () => {
+      disposed = true
+    }),
+    tap(failAfterDisposal, failAfterDisposal, failAfterDisposal),
+    take(3),
+    observe(value => {
+      actual.push(value)
+    }),
+  )
+
+  expect(actual).toEqual([10, 20, 30])
+  done()
 })
