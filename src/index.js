c7895e93c2901f73fae6806229b4cfeec208c059
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-07-19 14:35:05 +0200

    Cleanup values on disposal

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ export default function of(...values) {
     sink(0, type => {
       if (type !== 2) return
       disposed = true
+      values.length = 0
     })
 
     while (values.length !== 0) {
-      if (disposed) return
       sink(1, values.shift())
     }
+
+    if (disposed) return
 
     sink(2)
   }
