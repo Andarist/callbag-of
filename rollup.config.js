c257e6ae053886ed0b5725608d9d99b2c402a2d0
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-02-04 18:47:42 +0100

    Use named exports

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,8 @@ import pkg from './package.json'
 export default {
   input: 'src/index.js',
   output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' },
+    { file: pkg.main, format: 'cjs', exports: 'named' },
+    { file: pkg.module, format: 'esm' },
   ],
   plugins: [babel()],
 }
